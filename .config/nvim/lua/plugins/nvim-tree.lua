c1f1ddb95af2cd70dcd5f3201277d889892be95d
Nicolas Martinez V <nmartinezv@icloud.com>
2024-08-13 14:22:06 -0400

    feat(nvim): right align

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,50 +1,48 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    local ntree = require("nvim-tree")
+	"nvim-tree/nvim-tree.lua",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local ntree = require("nvim-tree")
 
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-    ntree.setup({
-      view = {
-        width = 35,
-        relativenumber = true,
-      },
+		ntree.setup({
+			view = {
+				width = 35,
+				relativenumber = false,
+				side = "right",
+			},
 
-      renderer = {
-        indent_markers = {
+			renderer = {
+				indent_markers = {
 
-          enable = true,
-        },
-        icons = {
-          glyphs = {
-            folder = {
-              arrow_closed = "→",
-              arrow_open = "↓",
-            },
-          },
-        },
-      },
-      actions = {
-        open_file = {
-          window_picker = {
-            enable = false, 
-          },
-        },
-      },
-      filters = {
-        custom = {
-          ".DS_Store",
-        },
-      },
-      git = {
-        ignore = false,
-      },
-    })
-
-
-   
-  end
+					enable = true,
+				},
+				icons = {
+					glyphs = {
+						folder = {
+							arrow_closed = "→",
+							arrow_open = "↓",
+						},
+					},
+				},
+			},
+			actions = {
+				open_file = {
+					window_picker = {
+						enable = false,
+					},
+				},
+			},
+			filters = {
+				custom = {
+					".DS_Store",
+				},
+			},
+			git = {
+				ignore = false,
+			},
+		})
+	end,
 }
