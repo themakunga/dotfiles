8344e3d77cc6bf9885a9e059c0e076c5260120a0
Nicolas Martinez V <nmartinezv@icloud.com>
2024-09-03 00:23:30 -0400

    feat(nvim): nvim-tree update conf

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -8,6 +8,8 @@ return {
 		vim.g.loaded_netrwPlugin = 1
 
 		ntree.setup({
+			update_cwd = true,
+			hijack_cursor = true,
 			view = {
 				width = 35,
 				relativenumber = false,
@@ -15,21 +17,43 @@ return {
 			},
 
 			renderer = {
+				highlight_git = true,
+				root_folder_modifier = ":t",
 				indent_markers = {
 
 					enable = true,
 				},
 				icons = {
 					glyphs = {
-						folder = {
-							arrow_closed = "→",
-							arrow_open = "↓",
+						default = "",
+						symlink = "",
+						bookmark = "◉",
+						git = {
+							unstaged = "",
+							staged = "",
+							unmerged = "",
+							renamed = "",
+							deleted = "",
+							untracked = "",
+							ignored = "",
 						},
+						folder = {
+							default = "",
+							open = "",
+							symlink = "",
+						},
+					},
+					show = {
+						git = false,
+						file = true,
+						folder = true,
+						folder_arrow = false,
 					},
 				},
 			},
 			actions = {
 				open_file = {
+					resize_window = true,
 					window_picker = {
 						enable = false,
 					},
@@ -44,5 +68,12 @@ return {
 				ignore = false,
 			},
 		})
+	end,
+	mappings = function()
+		local api = require("nvim-tree.api")
+
+		return {
+			{},
+		}
 	end,
 }
