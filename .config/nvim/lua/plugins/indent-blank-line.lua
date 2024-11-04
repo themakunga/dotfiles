1832297337e36a6fd21dab18e4e1d08c3a44fc7f
Nicolas Martinez V <nmartinezv@icloud.com>
2024-11-04 11:15:44 -0300

    fix(nvim): lazyraibow indent

--- .config/nvim/lua/plugins/indent-blank-line.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "lukas-reineke/indent-blankline.nvim",
-  events = { "BufReadPre", "BufNewFile" },
-  main = "ibl",
-  opts = function(_, opts)
-    return require("indent-rainbowline").make_opts(opts)
-  end,
-  dependencies = {"TheGLander/indent-rainbowline.nvim"},
-}
