06484fcf2264e7115bdebe801af53c9aac740b96
Nicolas Martinez V <nmartinezv@icloud.com>
2024-11-04 11:16:33 -0300

    feat(nvim): lazydev

---
+++ .config/nvim/lua/plugins/lazidev.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/lazydev.nvim",
+  ft = "lua",
+  dependencies = {
+    { "gonstoll/wezterm-types", lazy = true },
+    { "Bilal2453/luvit-meta", lazy = true },
+  },
+  opts = {
+    library = {
+      { path = "luvit-meta/library", words = { "vim%.uv" } },
+      { path = "wezterm-types", mods = { "wezterm" } },
+    },
+  },
+}
