2cc47a6e398f97cf93332ab37c5d5dcd42cbc484
Nicolas Martinez <nicolas.mv@globant.com>
2024-11-06 15:23:26 -0300

    feat(wezterm): config working

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,9 +11,7 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("Hack")
 config.font_size = 14
 config.color_scheme = "Tokyo Night Storm"
-config.enable_tab_bar = false
 
-config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 10
 
