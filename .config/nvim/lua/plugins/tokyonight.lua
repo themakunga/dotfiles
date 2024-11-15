0e406078c349a63fcbf8f2aad973d1529fe08734
Nicolas Martinez V <nmartinezv@icloud.com>
2024-11-15 12:13:41 -0300

    feat(nvim): theme transparent

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -6,8 +6,13 @@ return {
   config = function()
     require("tokyonight").setup({
       style = "night",
+      transparent = true,
+      styles = {
+        floats = "transparent",
+        sidebars = "transparent",
+      },
     })
 
     vim.cmd("colorscheme tokyonight")
-  end
+  end,
 }
